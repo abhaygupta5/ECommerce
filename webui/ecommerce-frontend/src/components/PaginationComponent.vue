efe6f6b3341ad560dc732aee13e223b199026c15
pragatimehrotra <mehrotra.pragati0@gmail.com>
2019-06-09 20:10:23 +0530

    Final frontend updated

---
+++ webui/ecommerce-frontend/src/components/PaginationComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="overflow-auto" id="pag">
+      <br>
+    <b-pagination-nav v-on:click.native="reloadPage" :link-gen="linkGen" :number-of-pages="numberOfPages()" use-router></b-pagination-nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PaginationComponent',
+    components:{
+      
+    },
+    data() {
+      return {
+        perPage: 3,
+        currentPage: 1
+      }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      }
+    },
+    methods: {
+      linkGen(pageNum) {
+        return pageNum === 1 ? '?' : `?page=${pageNum}`
+      },
+      reloadPage: function(data){
+        console.log("reloading", data)
+        // window.location.reload()
+      },
+      numberOfPages(){
+        return Math.ceil(this.$store.getters.getCategory.data.length/3)
+      }
+    }
+}
+
+</script>
+<style>
+#pag{
+    margin-left: 35%;
+}
+#current-page{
+  text-align: center;
+}
+</style>
---
+++ webui/ecommerce-frontend/src/components/Success.vue
@@ -0,0 +1,14 @@
+<template>
+<div style:="margin-left:40px">
+    <br/>
+    <br/>
+    <br/>
+    <h2>Your order is successfully placed. Thank you for shopping with us.</h2><br/>
+    <h2>You will soon receive a confirmation email from the merchant with order summary.</h2>
+    </div>
+</template>
+<script>
+export default {
+    name:'Success'
+}
+</script>
